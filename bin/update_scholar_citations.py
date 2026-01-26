31b299d23069fb52886ff551fd36656d57a6cfbc
akriegler <156933024+akriegler@users.noreply.github.com>
2026-01-26 19:44:47 +0100

    major udpate

--- bin/update_scholar_citations.py
+++ bin/update_scholar_citations.py
@@ -64,6 +64,7 @@ def get_scholar_citations() -> None:
 
     scholarly.set_timeout(15)
     scholarly.set_retries(3)
+    print('I got UID:', SCHOLAR_USER_ID)
     try:
         author = scholarly.search_author_id(SCHOLAR_USER_ID)
         author_data = scholarly.fill(author)
